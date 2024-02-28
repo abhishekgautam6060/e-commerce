7cad7e3a48602f34799d86fec934f80a900d2609
abhishekgautam6060 <111275009+abhishekgautam6060@users.noreply.github.com>
2024-02-28 22:52:29 +0530

    Update MainCaroselData.js

--- src/Customer/components/HomeCarosel/MainCaroselData.js
+++ src/Customer/components/HomeCarosel/MainCaroselData.js
@@ -7,4 +7,8 @@ export const mainCaroselData=[
         image:"https://www.forevernew.co.in//pub/media/wysiwyg/home/Womens-day-(Desktop).jpg",
         path:""
     },
+     {
+        image:"https://images.bloomingdalesassets.com/is/image/BcomMedia/media/0111_0117_HP_02_CAR_a_MultiFOB_VacationShop_1640641966085.jpg?scl=1&wid=1440",
+        path:""
+    }    
 ]
