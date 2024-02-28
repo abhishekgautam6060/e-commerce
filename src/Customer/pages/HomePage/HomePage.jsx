5bfbaf063b89e14c806a9138ab81feeee986be71
abhishekgautam6060 <111275009+abhishekgautam6060@users.noreply.github.com>
2024-02-28 23:18:55 +0530

    Update HomePage.jsx

--- src/Customer/pages/HomePage/HomePage.jsx
+++ src/Customer/pages/HomePage/HomePage.jsx
@@ -6,6 +6,7 @@ import React from 'react'
 import MainCrosel from '../../components/HomeCarosel/MainCrosel'
 import HomeSectionCarosel from '../../components/HomeSectionCarsoel/HomeSectionCarsoel'
 import { mens_kurta } from '../../../Data/mens_kurta'
+import { womens_kurta } from '../../../Data/womens_kurta'
 
 export const HomePage = () => {
   return (
@@ -16,6 +17,7 @@ export const HomePage = () => {
         
         <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
             <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Kurta"}/>
+            <HomeSectionCarosel data={womens_kurta} sectionName={"Men's Kurta"}/>
             <HomeSectionCarosel data={mens_kurta} sectionName={"Women's Kurtas"}/>
             <HomeSectionCarosel data={mens_kurta} sectionName={"Men's Shirt"}/>
             <HomeSectionCarosel data={mens_kurta} sectionName={"Women's Saree"}/>
